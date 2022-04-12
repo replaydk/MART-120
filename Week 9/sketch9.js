d4888b331bd9b2071219da887ec226324d186dc0
replaydk <98129711+replaydk@users.noreply.github.com>
2022-04-12 11:41:53 -0600

    Add files via upload

---
+++ Week 9/sketch9.js
@@ -0,0 +1,52 @@
+
+
+
+function setup() {
+createCanvas(1900, 400);
+        
+function draw() {
+background(0,0,255);
+textSize(72);
+text("The Necessities",700,100,);
+       
+        
+
+     
+rect (0,350,1900,100); //floor
+
+//Head
+strokeWeight(5);
+fill(0,255,200);
+circle (200,65,100);
+
+circle (100,150,50); //left arm
+circle (125,150,50);circle (150,150,50);circle (175,150,50);
+circle (300,150,50); //right arm
+circle (275,150,50); circle (250,150,50); circle (225,150,50);
+circle (300,350,50); //right leg"
+circle (275,325,50); circle (250,300,50); circle (225,275,50); circle (200,250,50);
+circle (100,350,50); //"left leg"
+circle (125,325,50);circle (150,300,50); circle (175,275,50);circle (200,250,50)
+circle (200,250,50); //center body
+circle (200,225,50); circle (200,200,50); circle (200,175,50);circle (200,150,50); circle (200,125,50);
+//door
+rect (400,50,250,350); 
+circle (600,250,50);
+textSize (36)
+text ("door" ,500,50);
+//pc
+rect(1400,200,150,200)
+triangle(1400,0,1550,150,1400,150)
+line(1500,150,1500,200)
+line(1450,150,1450,200)
+//Fridge
+rect(1700,100,200,300)
+text("Fridge", 1700,100)
+textSize(36)
+//signiture
+fill(0)
+text("MSI", 1440,250);
+textSize(24)
+text("Dean Klakken III", 1710,390)
+}
+    }
